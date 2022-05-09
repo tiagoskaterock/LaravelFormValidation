f9ef51f5d716c8e5cafd8322b56cc41838ee8a27
tiago <tiagolemespalhano@gmail.com>
2022-05-09 00:55:16 -0300

    81. HTML - Formulário Novo Cliente

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,5 +12,7 @@
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
--- resources/views/novo_cliente.blade.php
+++ resources/views/novo_cliente.blade.php
@@ -2,9 +2,43 @@
 
 @include('includes.header')
   
-<div class="container">
+<main role="main" class="container mt-5">
   
-</div>
+	<form method="post" action="#">
+
+
+
+		<div class="form-group">
+	    <label for="nome">Nome</label>
+	    <input type="text" class="form-control" id="nome" name="nome" aria-describedby="nome" placeholder="Nome completo">
+	  </div>
+
+
+		
+
+	  <div class="form-group">
+	    <label for="email">Email</label>
+	    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Seu melhor email">
+	  </div>
+
+
+
+
+
+
+	  <div class="form-group">
+	    <label for="endereco">Endereço</label>
+	    <input type="text" class="form-control" id="endereco" name="endereco" aria-describedby="Endereço" placeholder="Endereço, rua, número, bairro, apto...">
+	  </div>
+
+
+
+	  <button type="submit" class="btn btn-primary">Enviar</button>
+	  
+	</form>
+			
+
+</main>
 
 @include('includes.footer')
 		
